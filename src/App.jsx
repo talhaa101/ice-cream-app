4e25285664629079b841f36c5832b671cd0ffa5a
Abu Talha <talha007abu@gmail.com>
2026-01-22 15:11:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      Ice Cream Shop
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <div>
+        <h1>Welcome to the Ice Cream Shop!</h1>
+    </div>
+  );
+};
+
+export default Header;
